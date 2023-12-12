d210044d371207e48236a4458eee9295269dffcc
Zuong <quocvuongle.ct@gmail.com>
2023-12-12 20:32:24 +0700

    FourSum

---
+++ FourSum.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        return result;
+    }
+}
