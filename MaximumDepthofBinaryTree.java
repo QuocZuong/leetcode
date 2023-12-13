16a43cd37a788223711865c8e5571367689c728a
Zuong <quocvuongle.ct@gmail.com>
2023-12-13 20:27:53 +0700

    MaximumDepthofBinaryTree

---
+++ MaximumDepthofBinaryTree.java
@@ -0,0 +1,43 @@
+public class MaximumDepthofBinaryTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        int maxDepth = maxDepth(root);
+
+        System.out.println(maxDepth);
+    }
+}
