8fcd1a0adc440dd1bc4c8c8fb1e6f43582f9a32a
Zuong <quocvuongle.ct@gmail.com>
2023-12-13 20:12:01 +0700

    averageOfLevels

---
+++ AverageofLevelsinBinaryTree.js
@@ -0,0 +1,19 @@
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {};
+
+const root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
+
+console.log(averageOfLevels(root));
