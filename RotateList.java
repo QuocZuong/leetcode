fcadbcf97c6ba6cd0a38f5eb23a18fcd847082e4
Zuong <quocvuongle.ct@gmail.com>
2023-12-14 14:36:20 +0700

    rotate

---
+++ RotateList.java
@@ -0,0 +1,15 @@
+ class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+    
+public class RotateList {
+
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode dummy = head;
+        return dummy;
+    }
+}
