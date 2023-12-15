1c40103726a979cbcb8e4d917e29e8a34b084d4b
Zuong <quocvuongle.ct@gmail.com>
2023-12-15 10:05:21 +0700

    SymmetricTree

---
+++ SymmetricTree.java
@@ -0,0 +1,49 @@
+public class SymmetricTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        return recursive(root.left, root.right);
+    }
+
+    public static boolean recursive(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        } else {
+            return recursive(left.left, right.right) &&
+                    recursive(left.right, right.left);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+        root.right.left = new TreeNode(4);
+
+        System.out.println(isSymmetric(root));
+    }
+}
