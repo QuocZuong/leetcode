12c8c4cd68cc50cdcf3235325c75e5830e72ae8d
Zuong <quocvuongle.ct@gmail.com>
2023-12-29 20:22:44 +0700

    rotateRight

---
+++ RotateList.js
@@ -0,0 +1,23 @@
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function (head, k) {
+    while(){
+        
+    }
+
+    return head;
+};
+
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next = new ListNode(3);
+head.next = new ListNode(4);
+head.next = new ListNode(5);
