1a3b983c441594958cb35e9f641cffa6f80da3d3
Zuong <quocvuongle.ct@gmail.com>
2023-12-29 20:22:36 +0700

    merge

---
+++ MergeIntervals.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+    let result = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        for (let j = 0; j < intervals[i].length; j++) {}
+    }
+};
